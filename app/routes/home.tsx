72a401a033c4214f547749ed20187585cc22a94e
Kevin Chan <kevinmchan@live.com>
2025-02-05 19:55:28 -0500

    Remove react router filler content

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "buildingmycraft" },
+    { name: "description", content: "Welcome to my home base. It's where I work on my craft." },
   ];
 }
 
